9d79f23581e1a8be35b5ac7495ce6fe21e717a37
John Fornah <jfornah@gmail.com>
2023-07-31 20:05:37 -0700

    inital project setup.

---
+++ src/index.js
@@ -0,0 +1,38 @@
+/**
+ *  Project Brainstorming:
+ * 
+ *  - projects > todos > task desciptions
+ * 
+ *  - view/webpages:
+ *  - projects tabs
+ *  - todos for an individual project
+ *  - a space to edit description, due date, and priority
+ * 
+ *  - Todo object props:
+ *  - Title
+ *  - Discription
+ *  - dueDate
+ *  - Priority
+ *  - complete? 
+ * 
+ *  - initally, Project 1 could be a default project, where all todos initally go
+ *      - allow the user to change the default?
+ * 
+ *  - Modulars needed (separate dom and application logic):
+ *  - create new todo (logic)
+ *  - setting todo as complete (logic)
+ *  - changing todo priority (logic)
+ *  - changing views (logic? dom)
+ *      
+ *  - UI Requirments:
+ *      - view all projects
+ *      - view all todos in a individual project
+ *      - expand a todo to see/edit details
+ *      - delete a todo
+ *      - delete a project ( add safety )
+ * 
+ *  - UI design:
+ *      - Im thinking of having the ui look similar to trello
+ *      - Projects view will be a grid of all projects
+ *      - individual project view will look like a trello board
+ */
